23a717af02c2621b9a185d40a8f72e8595df140e
PremchandSingh <singhpremchand299@gmail.com>
2025-03-05 01:43:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 # from snowflake.snowpark.context import get_active_session
 import requests
+import pandas as pd_df
 
 
 # cnx = st.connection("Snowflake")
