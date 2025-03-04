b61879c0f1130d7f1cb5c4604c8b7357f73760b5
PremchandSingh <singhpremchand299@gmail.com>
2025-03-04 20:22:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,11 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col, when_matched
-# from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.context import get_active_session
 
 
-cnx = st.connection("Snowflake")
-session =cnx.session()
+# cnx = st.connection("Snowflake")
+# session =cnx.session()
 
 # Write directly to the app
 st.title("Example Streamlit App :balloon:")
