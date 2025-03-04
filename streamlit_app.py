3f90f5eec627d281ef355fd4d5078d48c2b4772f
PremchandSingh <singhpremchand299@gmail.com>
2025-03-05 01:12:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,6 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 # from snowflake.snowpark.context import get_active_session
 
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
 
 # cnx = st.connection("Snowflake")
 # session =cnx.session()
@@ -55,6 +52,10 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
 
+
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response.json())
     
 
 
